127c5f756a78a516a3abd15003d4e290369b2aff
renataintec <renata.dayana@gmail.com>
2021-08-25 23:25:46 -0300

    Componentes do blog

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import * as core from '@angular/core';
+
+@core.Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements core.OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
