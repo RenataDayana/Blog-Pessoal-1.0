3e8e2d3d173af44f54d613403311431fbe7c956d
renataintec <renata.dayana@gmail.com>
2021-09-02 11:35:14 -0300

    Implementação do Get e Post do tema

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -30,10 +30,7 @@ export class EntrarComponent implements OnInit {
       environment.foto = this.usuarioLogin.foto
       environment.id = this.usuarioLogin.id
 
-      // console.log(environment.token)
-      // console.log(environment.nome)
-      // console.log(environment.foto)
-      // console.log(environment.id)
+      console.log(environment)
 
       this.router.navigate(["/inicio"])         
     }, erro =>{
